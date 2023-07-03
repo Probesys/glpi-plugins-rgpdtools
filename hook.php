f9f458cd1f27b253e2993272adad0fb91e942f38
Philippe GODOT <philippe.godot@probesys.com>
2023-07-03 14:51:03 +0200

    fxe licence problem

--- hook.php
+++ hook.php
@@ -5,7 +5,7 @@
  *  ---------------------------------------------------------------------
  *  LICENSE
  *
- *  This file is part of rgpdTools. ppppppppppp
+ *  This file is part of rgpdTools.
  *
  *  rgpdTools is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
